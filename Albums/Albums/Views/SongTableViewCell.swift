527b83f97cd95769425df97cb423c4414f584376
Linh Bouniol <paulalinh89@gmail.com>
2018-08-31 15:02:06 -0700

    Created storyboards

---
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Linh Bouniol on 8/31/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var songTitleTextField: UITextField!
+    @IBOutlet weak var songDurationTextField: UITextField!
+    @IBOutlet weak var addSong: UIButton!
+    
+    @IBAction func addSong(_ sender: Any) {
+    }
+}
