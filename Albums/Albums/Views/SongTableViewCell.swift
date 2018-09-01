f5b39595c32239a2e48c23d5f75f415f9e21f08a
Linh Bouniol <paulalinh89@gmail.com>
2018-09-01 15:29:25 -0700

    Created song table view cell

--- Albums/Albums/Views/SongTableViewCell.swift
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -8,12 +8,43 @@
 
 import UIKit
 
+protocol SongTableViewCellDelegate: class {
+    func addSong(with title: String, duration: String)
+}
+
 class SongTableViewCell: UITableViewCell {
 
+    var song: Song? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    weak var delegate: SongTableViewCellDelegate?
+    
     @IBOutlet weak var songTitleTextField: UITextField!
     @IBOutlet weak var songDurationTextField: UITextField!
     @IBOutlet weak var addSong: UIButton!
     
     @IBAction func addSong(_ sender: Any) {
+        guard let title = songTitleTextField.text, let duration = songDurationTextField.text else { return }
+        
+        delegate?.addSong(with: title, duration: duration)
+    }
+    
+    func updateViews() {
+        guard let song = song else {
+            addSong.alpha = 1.0
+            return }
+        
+        addSong.alpha = 0.0
+            
+        songTitleTextField.text = song.title
+        songDurationTextField.text = song.duration
+    }
+    
+    override func prepareForReuse() {
+        songTitleTextField.text = nil
+        songDurationTextField.text = nil
     }
 }
