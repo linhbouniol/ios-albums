3dde7a9a50002c97d1c27c14bdd720f3d94c8506
Linh Bouniol <paulalinh89@gmail.com>
2018-09-01 11:30:32 -0700

    Finished decoding

---
+++ Albums/Albums/Models/Album.swift
@@ -0,0 +1,67 @@
+//
+//  Album.swift
+//  Albums
+//
+//  Created by Linh Bouniol on 8/31/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import Foundation
+
+struct Album: Decodable {
+    
+    let name: String
+    let artist: String
+    let id: String
+    let genres: [String]
+    let coverArt: [URL]
+    let songs: [Song]
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case artist
+        case id
+        case genres
+        case coverArt
+        case songs
+        
+        enum CoverArtCodingKeys: String, CodingKey {
+            case url
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // These would have been decoded automatically, if there was no coverArt.
+        self.name = try container.decode(String.self, forKey: .name)
+        self.artist = try container.decode(String.self, forKey: .artist)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.genres = try container.decode([String].self, forKey: .genres)
+        self.songs = try container.decode([Song].self, forKey: .songs)
+        
+        var coverArtContainer = try container.nestedUnkeyedContainer(forKey: .coverArt)
+        
+        var coverArt = [URL]()
+        
+        while !coverArtContainer.isAtEnd {
+            let urlContainer = try coverArtContainer.nestedContainer(keyedBy: CodingKeys.CoverArtCodingKeys.self)
+            
+            let urlString = try urlContainer.decode(String.self, forKey: .url)
+            
+            // if this string is not a url, then you continue to the next url, return will not work here, b/c you need to return something back since we're in an init(). Throw would be better but more complicated to set up.
+            guard let url = URL(string: urlString) else { continue }
+            
+            coverArt.append(url)
+        }
+        
+//        self.name = name
+//        self.artist = artist
+//        self.id = id
+//        self.genres = genres // keep as array here, join with commas in the VC
+//        self.songs = songs
+        self.coverArt = coverArt
+    }
+    
+}
---
+++ Albums/Albums/Models/Song.swift
@@ -0,0 +1,48 @@
+//
+//  Song.swift
+//  Albums
+//
+//  Created by Linh Bouniol on 8/31/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import Foundation
+
+struct Song: Decodable {
+    
+    let title: String
+    let duration: String
+    let id: String
+    
+    enum CodingKeys: String, CodingKey {
+        case duration
+        case name
+        case id
+        
+        enum DurationCodingKeys: String, CodingKey {
+            case duration
+        }
+        
+        enum NameCodingKeys: String, CodingKey {
+            case title
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id = try container.decode(String.self, forKey: .id)
+        
+        let durationContainer = try container.nestedContainer(keyedBy: CodingKeys.DurationCodingKeys.self, forKey: .duration)
+        self.duration = try durationContainer.decode(String.self, forKey: .duration)
+        
+        let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.NameCodingKeys.self, forKey: .name)
+        self.title = try nameContainer.decode(String.self, forKey: .title)
+        
+//        self.id = id
+//        self.duration = duration
+//        self.title = title
+    }
+    
+}
